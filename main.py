fb48380bbd159db5b939674b98974c955867bd25
unknown <rodgers.forrest3645@gmail.com>
2026-05-02 16:22:18 -0700

    add gitignore and first probablitiy matrix attempt.

--- main.py
+++ main.py
@@ -9,8 +9,20 @@ white_to_red = LinearSegmentedColormap.from_list("wtr", ["white", "red"])
 grid = freqency_grid.get_grid()
 prob = propagate.propagate(grid)
 
-plt.imshow(prob,
+fig, (ax1, ax2) = plt.subplots(1, 2)
+im1 = ax1.imshow(grid,
             cmap=white_to_red,
             interpolation='nearest')
-plt.colorbar()
+ax1.set_title("Frequency Grid")
+
+fig.colorbar(im1, ax=ax1, fraction=0.046, pad=0.04)
+
+im2 = ax2.imshow(prob,
+            cmap=white_to_red,
+            interpolation='nearest')
+ax2.set_title("Propagated Probability")
+
+fig.colorbar(im2, ax=ax2, fraction=0.046, pad=0.04)
+
+
 plt.show()
